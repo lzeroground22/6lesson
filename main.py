611ee5e23b3f81cc4150e8c8a7af17a261faf367
Alex <User@Big-PC.les.local>
2021-04-02 23:56:14 +0300

    add 2 class

--- main.py
+++ main.py
@@ -23,3 +23,12 @@ class Mentor:
         else:
             return 'Ошибка'
 
+
+class Lecturer(Mentor):
+    def grade_collect(self):
+        pass
+
+
+class Reviewer(Mentor):
+    def grade_collect(self):
+        pass
